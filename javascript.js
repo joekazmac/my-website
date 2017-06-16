7a868d8b645bcd0e9cf4c54dbb8f02f4f16ca11d
SJV <sjv@yogamp05yl2j.sjv.local>
2017-06-15 20:25:38 -0400

    jQuery work

--- javascript.js
+++ javascript.js
@@ -1,16 +1,47 @@
 $( document ).ready(function() {
-	var url = window.location.href;
-	console.log(url)
+	// var url = window.location.href;
+	// console.log(url)
 
-	$("#headerstyles").click(function( event ) {
-		alert( "Image Clicked")
+	// $("#headerstyles").click(function( event ) {
+	// 	alert( "Image Clicked")
+	// });
+
+	// $("h2").click(function(){
+	// 	console.log($("h2").text())
+	// });
+	// $("h3").click(function() {
+	// 	$( "h3").last().addClass("addingClass")
+	// });
+
+	var addOrRemoveConditionalClass = function(ele){
+
+	
+		if (ele.hasClass("addingClass")) {
+			ele.removeClass("addingClass");
+		}
+		else{
+			ele.addClass("addingClass")
+		}
+	}
+
+	$("#header2styles").click(function( event ) {
+		var ele = $( "#header2styles" );
+		addOrRemoveConditionalClass(ele)
+	})
+
+	$("#backgroundcolor").ready(function(){
+	$("#backgroundcolor").addClass("change")
+
+	$("#headerstyles").addClass("h1change")
+
 	});
 
-	$("h2").click(function(){
-		console.log($("h2").text())
-	});
-	$("h3").click(function() {
-		$( "h3").last().addClass("addingClass")
+$("#styling1").click(function(){
+	$("#styling1").append("<p id='name-tag'>Joe Kazmac</p>");
+	$("#name-tag").click(function(){
+        alert($(this).text());
+		});
+
 	});
 
 });
